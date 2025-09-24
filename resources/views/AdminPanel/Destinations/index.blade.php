a2f5ba30ccc71e88b59b60718d9769bf6706fc1f
SUNNAH <shahriasunnah0@gmail.com>
2025-09-24 11:17:31 -0700

    Update

--- resources/views/AdminPanel/Destinations/index.blade.php
+++ resources/views/AdminPanel/Destinations/index.blade.php
@@ -51,7 +51,7 @@
     <div class="container mt-4">
             <div class="card shadow-lg">
                 <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-                    <h4 class="mb-0">Hotel Management</h4>
+                    <h4 class="mb-0">Destination Management</h4>
         <button class="btn btn-success mb-3" onclick="openCreateModal()">+ Add New Location</button>
 
                 </div>
